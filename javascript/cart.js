09a956a845db47ea81a0efe28a819d487a39ce28
Tomatsuppe11211 <marcos.lindboe47@gmail.com>
2024-11-12 00:05:27 +0100

    Mini remake
    I have tried to fix the cart so i can remove a chosen product from the cart. For now i think it is closer to my goal, but deleting an object isnt quite helping. I will look for another way

--- javascript/cart.js
+++ javascript/cart.js
@@ -5,6 +5,15 @@ let cart = JSON.parse(localStorage.getItem("products"))
 
 
 /*Creating a message for the user if no products are detevted*/
+
+
+
+let sum = 0;
+
+
+/*Marking where i want to put the users products*/
+const cartSection = document.getElementById("displayCart")
+
 function message(){
     const message = document.createElement("p")
     message.innerHTML = "There are no products in your cart"
@@ -12,75 +21,70 @@ function message(){
 }
 
 
-let sum = 0;
 
-
-
-
-
-/*Marking where i want to put the users products*/
-const cartSection = document.getElementById("displayCart")
-
-
-//making message if cart is empty
-function giveMessage(){
-    const message = document.createElement("p")
-    message.innerHTML = "Your cart is empty"
-    cartSection.appendChild(message)
-}
-
-
-if (cart != null){
+if (cart.length != null){
+    
     for(let i = 0; i < cart.length; i++){
-        /*Adding the products to the cart page*/
-        const cartinfo = document.createElement("div")
-            cartinfo.className = "cartInfo"
+        //createing the elements for each product in our cart
+        const list = document.createElement("div")
+            list.className = "cartInfo"
         const gameImage = document.createElement("img")
             gameImage.src = cart[i].image
-        const gameInfo = document.createElement("div") 
-            gameInfo.className = "product" 
+        const gameInfo = document.createElement("div")
+            gameInfo.className = "product"
         const gameTitle = document.createElement("h3")
             gameTitle.innerHTML = cart[i].name
         const gamePrice = document.createElement("p")
-            if(cart[i].sale == true){
+            if(cart[i].sale === true){
                 gamePrice.innerHTML = "Price: " + cart[i].discount + " $"
                 sum += cart[i].discount
             } else {
-                gamePrice.innerHTML = "price: " + cart[i].price + " $"
+                gamePrice.innerHTML = "Price: " + cart[i].price + " $"
                 sum += cart[i].price
             }
         const trashIcon = document.createElement("i")
             trashIcon.className = "fa-solid fa-trash"
-    
-    
-        cartSection.appendChild(cartinfo)
-        cartinfo.appendChild(gameImage)
-        cartinfo.appendChild(gameInfo)
-        gameInfo.appendChild(gameTitle)
-        gameInfo.appendChild(gamePrice)
-        cartinfo.appendChild(trashIcon)
 
-        trashIcon.onclick = function(){
-            /*if(cart[i].sale == true){
-                sum -= cart[i].discount
-            } else {
-                sum -= cart[i].price
+        //displaying all products to the page
+        cartSection.appendChild(list)
+            list.appendChild(gameImage)
+            list.appendChild(gameInfo)
+                gameInfo.appendChild(gameTitle)
+                gameInfo.appendChild(gamePrice)
+            list.appendChild(trashIcon)
+
+
+        //Removing items from cart
+        trashIcon.onclick = function() {
+            console.log(cart[i].name + " removed")
+            delete cart[i]
+
+            if(cart[i] == "empty"){
+                cart[i] = cart[0]
+                cart.shift()    
             }
-            console.log(sum.toFixed(2))*/
-
-            //cartinfo.remove()
-            cart.splice(i-2, i)
+            
             console.log(cart)
+            
+            list.remove()
 
             //localStorage.clear("products")
             //localStorage.setItem("products", JSON.stringify(cart))
-        } 
-    }     
+            
+            
+        }
+
+    }
+
+    console.log(sum.toFixed(2))
+
+
+
 } else {
-    giveMessage()
+    message()
 }
 
 
-console.log(cart)
 
-console.log(sum.toFixed(2))
+
+console.log(cart)
